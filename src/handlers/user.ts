6bb7773aeaf1e861291ba12d2ff2288e1d852448
Krupesh Anadkat <kmanadkat@gmail.com>
2023-03-23 01:59:22 +0530

    feat: Create New User & Generate JWT Token

---
+++ src/handlers/user.ts
@@ -0,0 +1,15 @@
+import prisma from '../db';
+import { createJWT, hashPassword } from '../modules/auth';
+
+export const createNewUser = async (req, res) => {
+	const hash = await hashPassword(req.body.password);
+	const user = await prisma.user.create({
+		data: {
+			username: req.body.username,
+			password: hash,
+		},
+	});
+
+	const token = createJWT(user);
+	return res.json({ token });
+};
