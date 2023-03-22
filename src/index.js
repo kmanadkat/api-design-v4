ee9389539f8c8e25d1b0838559d9105e1fe535c6
Krupesh Anadkat <kmanadkat@gmail.com>
2023-03-22 21:39:49 +0530

    feat: Express Server

--- src/index.js
+++ src/index.js
@@ -1,18 +1,7 @@
-const http = require('http')
-
-const server = http.createServer((req, res) => {
-  if (req.url === '/' && req.method === 'GET') {
-    res.writeHead(200, 'OK', { "Content-Type": "application/json" })
-    res.write(JSON.stringify({ message: "hello world" }))
-    res.end();
-    return;
-  }
-  res.writeHead(404, 'Not Found', { "Content-Type": "application/json" })
-  res.end(JSON.stringify({ message: "nope" }));
-})
+const app = require("./server");
 
 const PORT = process.env.PORT
 
-server.listen(PORT, () => {
-  console.log(`server on ${PORT}`);
+app.listen(PORT, () => {
+  console.log(`🚀 listening on ${PORT}`)
 })
