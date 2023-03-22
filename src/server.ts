9d592100ac23c32007b2c9b10d85af554512b6d3
Krupesh Anadkat <kmanadkat@gmail.com>
2023-03-23 00:55:49 +0530

    feat: Added Wildcard Cors Middleware

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,13 @@
 import express from 'express';
 import morgan from 'morgan';
+import cors from 'cors';
 import router from './router';
 
 const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use(morgan('dev'));
 
