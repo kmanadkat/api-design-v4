5b03bc779ccba8a766cac160e0d510645055d947
Krupesh Anadkat <kmanadkat@gmail.com>
2023-03-23 11:30:27 +0530

    feat: Route Input Validations Added

--- src/router.ts
+++ src/router.ts
@@ -1,4 +1,6 @@
 import { Router } from 'express';
+import { body } from 'express-validator';
+import { handleInputErrors } from './modules/middleware';
 
 const router = Router();
 
@@ -11,9 +13,19 @@ router.get('/products', (req, res) => {
 
 router.get('/products/:id', (req, res) => {});
 
-router.post('/products', (req, res) => {});
+router.post(
+	'/products',
+	body('name').isString(),
+	handleInputErrors,
+	(req, res) => {}
+);
 
-router.put('/products/:id', (req, res) => {});
+router.put(
+	'/products/:id',
+	body('name').isString(),
+	handleInputErrors,
+	(req, res) => {}
+);
 
 router.delete('/products/:id', (req, res) => {});
 
@@ -24,9 +36,23 @@ router.get('/updates', (req, res) => {});
 
 router.get('/updates/:id', (req, res) => {});
 
-router.post('/updates', (req, res) => {});
+router.post(
+	'/updates',
+	body('title').isString(),
+	body('body').isString(),
+	handleInputErrors,
+	(req, res) => {}
+);
 
-router.put('/updates/:id', (req, res) => {});
+router.put(
+	'/updates/:id',
+	body('title').isString().optional(),
+	body('body').isString().optional(),
+	body('status').isIn(['IN_PROGRESS', 'SHIPPED', 'DEPRECATED']).optional(),
+	body('version').isString().optional(),
+	handleInputErrors,
+	(req, res) => {}
+);
 
 router.delete('/updates/:id', (req, res) => {});
 
@@ -37,9 +63,22 @@ router.get('/updatepoints', (req, res) => {});
 
 router.get('/updatepoints/:id', (req, res) => {});
 
-router.post('/updatepoints', (req, res) => {});
+router.post(
+	'/updatepoints',
+	body('name').isString(),
+	body('description').isString(),
+	body('updateId').isString(),
+	handleInputErrors,
+	(req, res) => {}
+);
 
-router.put('/updatepoints/:id', (req, res) => {});
+router.put(
+	'/updatepoints/:id',
+	body('name').isString().optional(),
+	body('description').isString().optional(),
+	handleInputErrors,
+	(req, res) => {}
+);
 
 router.delete('/updatepoints/:id', (req, res) => {});
 
